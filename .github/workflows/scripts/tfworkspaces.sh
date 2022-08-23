9879b1830288e6020281c5b016c273542a7d0a64
Admin Umar Khan <admin.ukhan@cudirect.com>
2022-08-23 16:52:17 -0700

    skip title and dashes

--- .github/workflows/scripts/tfworkspaces.sh
+++ .github/workflows/scripts/tfworkspaces.sh
@@ -11,11 +11,16 @@ ls
 # for workspace in $workspaceData
 cat ws.txt | while read workspace;
 do 
-    echo $workspace
-    api=$(curl \
-    --header "Authorization: Bearer $tfe_token" \
-    --header "Content-Type: application/vnd.api+json" \
-    "$workspacesUrl/$workspace" | jq '.data.attributes.name, "|", .data.relationships."current-run".data, "|", .data.relationships."latest-run".data, "|", .data.relationships."current-state-version".data' )
-    
-    echo $api >> tfresponse.json
+    if ($workspace == "wsname" || $workspace == "------")
+    then
+        echo "not a workspace"
+    else
+        echo $workspace
+        api=$(curl \
+        --header "Authorization: Bearer $tfe_token" \
+        --header "Content-Type: application/vnd.api+json" \
+        "$workspacesUrl/$workspace" | jq '.data.attributes.name, "|", .data.relationships."current-run".data, "|", .data.relationships."latest-run".data, "|", .data.relationships."current-state-version".data' )
+        
+        echo $api >> tfresponse.json
+    fi
 done
