e0eb9038d0fe79a3e49307edae504d8c960f31cf
Admin Umar Khan <admin.ukhan@cudirect.com>
2022-08-23 17:16:36 -0700

    skip title and dashes

--- .github/workflows/scripts/tfworkspaces.sh
+++ .github/workflows/scripts/tfworkspaces.sh
@@ -7,7 +7,6 @@ echo $workspacesUrl
 echo "workspaceName | current-run | latest-run | current-state-version" >> tfresponse.json
 echo "----------------------------------------------------------" >> tfresponse.json
 
-ls
 # for workspace in $workspaceData
 cat ws.txt | while read workspace;
 do 
@@ -15,8 +14,8 @@ do
         echo "not a workspace"
     elif [[ "$workspace" == "------" ]]; then
         echo "lines in the file"
-    elif [[ ! -z "$workspace" ]]; then
-        echo "empty line"
+    # elif [[ ! -z "$workspace" ]]; then
+    #     echo "empty line"
     else
         echo $workspace
         # api=$(curl \
