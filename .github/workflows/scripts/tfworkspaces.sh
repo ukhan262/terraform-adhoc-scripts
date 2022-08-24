bbdd5ad1d6fd713e65df709e6f5f5cb57e072bfa
Admin Umar Khan <admin.ukhan@cudirect.com>
2022-08-23 17:19:27 -0700

    skip title and dashes

--- .github/workflows/scripts/tfworkspaces.sh
+++ .github/workflows/scripts/tfworkspaces.sh
@@ -14,8 +14,8 @@ do
         echo "not a workspace"
     elif [[ "$workspace" == "------" ]]; then
         echo "lines in the file"
-    # elif [[ ! -z "$workspace" ]]; then
-    #     echo "empty line"
+    elif [[ ! -z "$workspace" ]]; then
+        echo "empty line"
     else
         echo $workspace
         api=$(curl \
